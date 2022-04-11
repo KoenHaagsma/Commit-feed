0c1157a57c199418594f2fb0c4e49ca40ef68f01
Lotte Koblens <lotte.koblens@hva.nl>
2022-04-11 12:26:27 +0200

    setup profile page

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const PORT = 3000;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+    res.render('index')
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+
+app.get('/scores', (req, res) => {
+    res.render('scores')
+})
 
 app.use((req, res) => {
     res.status(404).render('error404');
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Application started on port: http://localhost:${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Application started on port: http://localhost:${PORT}`);
 });
