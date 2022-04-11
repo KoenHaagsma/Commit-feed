2171eb1b995c160f12fac8632a52bfae5a28c32f
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-04-11 12:17:43 +0200

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
+const PORT = 4000
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
-    res.render('index');
-});
+	res.render('index')
+})
+
+app.get('/profile', (req, res) => {
+	res.render('profile')
+})
+
+app.get('/scores', (req, res) => {
+	res.render('scores')
+})
 
 app.use((req, res) => {
-    res.status(404).render('error404');
-});
+	res.status(404).render('error404')
+})
 
-app.listen(process.env.PORT, () => {
-    console.log(`Application started on port: http://localhost:${process.env.PORT}`);
-});
+app.listen(PORT, () => {
+	console.log(`Application started on port: http://localhost:${PORT}`)
+})
