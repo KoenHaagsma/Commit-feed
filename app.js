8116565cd9cb0a40056bae9affb3f4f524bffa94
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-04-12 12:13:04 +0200

    Add logic for person/repo stats

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const graphqlAuth = graphql.defaults({
 
 const app = express();
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-	res.render('index')
-})
+    res.render('index');
+});
 
 app.get('/profile', (req, res) => {
-	res.render('profile')
-})
+    res.render('profile');
+});
 
 app.get('/score', (req, res) => {
     graphqlAuth(`query MyQuery {
@@ -60,27 +60,74 @@ app.get('/score', (req, res) => {
             }
         }
     }`)
-        .then(data => {
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.owner.login);
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges.length);
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.repository.name)
+        .then((data) => {
+            const stats = [];
+            const names = [];
+            const baseURL = data.organization.repositories.edges[0].node.forks.edges;
 
-            const dataSet = {
-                ownerName = data.organization.repositories.edges[0].node.forks.edges[0].node.owner.login,
-                lengthCommits = data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges.length
-            }
+            // Add each author to names array to lowercase and replace spaces with underscores
+            baseURL.forEach((user) => {
+                user.node.defaultBranchRef.target.history.edges.forEach((branch) => {
+                    names.push({
+                        name: branch.node.author.name.toLowerCase().replace(/ /g, '_'),
+                    });
+                });
+            });
 
-            res.render('index', { dataSet });
+            // Count commit messages per student
+            const count = names.reduce((sums, entry) => {
+                sums[entry.name] = (sums[entry.name] || 0) + 1;
+                return sums;
+            }, {});
+
+            // Sort out teachers in count object
+            const asArrayStudents = Object.entries(count);
+            const filtered = asArrayStudents.filter(([key, value]) => {
+                return !(key === 'robert_spier' || key === 'ju5tu5' || key === 'justus_sturkenboom');
+            });
+            const justStudents = Object.fromEntries(filtered);
+            const sortedJustStudents = sortBasedOnValue(justStudents);
+
+            baseURL.forEach((repo) => {
+                stats.push({
+                    [`${repo.node.owner.login}/${repo.node.defaultBranchRef.repository.name}`]:
+                        repo.node.defaultBranchRef.target.history.edges.length,
+                });
+            });
+
+            const allStats = Object.assign({}, ...stats);
+            const sortedAllStats = sortBasedOnValue(allStats);
+
+            res.render('scores.ejs', { person_commit_count: sortedAllStats, person_commit_count: sortedJustStudents });
         })
-        .catch(err => console.log(err))
-
-    res.render('index');
+        .catch((err) => {
+            console.error(err);
+        });
 });
 
 app.use((req, res) => {
-	res.status(404).render('error404')
-})
+    res.status(404).render('error404');
+});
 
 app.listen(process.env.PORT, () => {
-	console.log(`Application started on port: http://localhost:${process.env.PORT}`)
-})
+    console.log(`Application started on port: http://localhost:${process.env.PORT}`);
+});
+
+// Sort Keys based on values given
+function sortBasedOnValue(obj) {
+    const sortable = [];
+    const sortedArray = [];
+    for (var single in obj) {
+        sortable.push([single, obj[single]]);
+    }
+
+    sortable.sort((a, b) => {
+        return b[1] - a[1];
+    });
+
+    sortable.forEach((key, value) => {
+        sortedArray.push({ name: key[0], count: key[1] });
+    });
+
+    return sortedArray;
+}
