1635ddab6aa6c0c5e5e58152935e395f2f7360ed
dannyfrelink <danny-frelink@hotmail.com>
2022-04-12 11:43:08 +0200

    new data query

--- app.js
+++ app.js
@@ -12,76 +12,53 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/profile/:author', (req, res) => {
     graphqlAuth(`query MyQuery {
-        organization(login: "cmda-minor-web") {
+        user(login: "${req.params.author}") {
           name
-          repositories(last: 1) {
+          bioHTML
+          avatarUrl
+          repositories(orderBy: {field: CREATED_AT, direction: DESC}, first: 100) {
             edges {
               node {
                 name
-                forks(first: 100) {
-                  edges {
-                    node {
-                      owner {
-                        login
-                      }
-                      defaultBranchRef {
-                        repository {
-                          name
-                        }
-                        target {
-                          ... on Commit {
-                            history(first: 100) {
-                              edges {
-                                node {
-                                  author {
-                                    name
-                                    user {
-                                      avatarUrl
-                                      bioHTML
-                                      repositories(last: 10) {
-                                        edges {
-                                          node {
-                                            name
-                                            description
-                                            url
-                                            updatedAt
-                                          }
-                                        }
-                                      }
-                                    }
-                                  }
-                                  message
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
+                description
+                url
+                updatedAt
               }
             }
           }
         }
       }`)
         .then(data => {
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.name);
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.avatarUrl);
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.bioHTML);
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.name)
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.description)
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.url)
-            console.log(data.organization.repositories.edges[0].node.forks.edges[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.updatedAt)
+            const repoArray = [];
+            data.user.repositories.edges.forEach(repo => {
+                return repoArray.push(repo.node)
+            });
+            const dataSet = {
+                authorName: data.user.name,
+                avatarUrl: data.user.avatarUrl,
+                bioHTML: data.user.bioHTML,
+                profileRepoName: data.user.repositories,
+                // profileRepoDescription: baseUrl[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.description,
+                // profileRepoUrl: baseUrl[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.url,
+                // profileRepoLastUpdate: baseUrl[0].node.defaultBranchRef.target.history.edges[0].node.author.user.repositories.edges[0].node.updatedAt
+            }
 
-            res.render('index');
+            // console.log(dataSet.authorName)
+            // console.log(dataSet.avatarUrl)
+            // console.log(dataSet.bioHTML)
+            // console.log(dataSet.profileRepoName)
+            // console.log(dataSet.profileRepoDescription)
+            // console.log(dataSet.profileRepoUrl)
+            // console.log(dataSet.profileRepoLastUpdate)
+
+            res.render('profile');
         })
         .catch(err => console.log(err))
-})
-
-app.get('/profile', (req, res) => {
-    res.render('profile')
 })
 
 app.get('/score', (req, res) => {
